ad6389f1972bf965d2313a5d27e7a595a3935a83
lrm <lrm@gonsin.cn>
2023-12-07 15:48:58 +0800

    adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/font';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Dashboard',
+    default: 'Dashboard',
+  },
+  description: 'Dashboard sample by nextjs',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
